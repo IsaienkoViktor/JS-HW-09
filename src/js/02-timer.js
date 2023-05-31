ade2f757f75c6e0202a117d71d6378645eb9a4d9
IsaienkoViktor <isaenkov1986@gmail.com>
2023-05-31 17:05:51 +0300

    add 2-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,8 +7,8 @@ const refs = {
     startBtn: document.querySelector('button[data-start]'),
     days: document.querySelector('span[data-days]'),
     hours: document.querySelector('span[data-hours]'),
-    mins: document.querySelector('span[data-minutes]'),
-    secs: document.querySelector('span[data-seconds]'),
+    minutes: document.querySelector('span[data-minutes]'),
+    seconds: document.querySelector('span[data-seconds]'),
   };
 
 refs.startBtn.disabled = true;
@@ -28,11 +28,12 @@ const options = {
     refs.startBtn.addEventListener('click', () => {
   intervalId = setInterval(() => {
     const diff = selectedDates[0] - Date.now();
+    refs.startBtn.disabled = true;
     if (diff <= 1000) {
-      clearInterval(intervalId);
+      clearInterval(intervalId);    
     }
     const result = convertMs(diff);
-    liveTimer(result);
+    Timer(result);
   }, 1000);
 });
 },
@@ -41,11 +42,11 @@ const options = {
 
 flatpickr('#datetime-picker', options);
 
-function liveTimer({ days, hours, minutes, seconds }) {
+function Timer({ days, hours, minutes, seconds }) {
   refs.days.textContent = `${days}`;
   refs.hours.textContent = `${hours}`;
-  refs.mins.textContent = `${minutes}`;
-  refs.secs.textContent = `${seconds}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
 }
 
 function addLeadingZero(value) {
@@ -61,13 +62,13 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
@@ -76,78 +77,6 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-// import Notiflix from 'notiflix';
 
-// const refs = {
-//   input: document.querySelector('#datetime-picker'),
-//   start: document.querySelector('button[data-start]'),
-//   days: document.querySelector('span[data-days]'),
-//   hours: document.querySelector('span[data-hours]'),
-//   mins: document.querySelector('span[data-minutes]'),
-//   secs: document.querySelector('span[data-seconds]'),
-// };
-
-// let intervalId = null;
-// refs.start.disabled = true;
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-
-//     if (selectedDates[0] < new Date()) {
-//       refs.start.disabled = true;
-//       Notiflix.Notify.failure('Please choose a date in the future! Do not look back..');
-//       return;
-//     }
-//     if (selectedDates[0] > new Date()) {
-//       refs.start.disabled = false;
-//     }
-
-//     refs.start.addEventListener('click', () => {
-//       intervalId = setInterval(() => {
-//         const differenceInTime = selectedDates[0] - new Date();
-
-//         if (differenceInTime < 1000) {
-//           clearInterval(intervalId);
-//         }
-//         const result = convertMs(differenceInTime);
-//         viewOfTimer(result);
-//       }, 1000);
-//     });
-//   },
-// };
-
-// flatpickr('#datetime-picker', options);
-
-// function viewOfTimer({ days, hours, minutes, seconds }) {
-//   refs.days.textContent = `${days}`;
-//   refs.hours.textContent = `${hours}`;
-//   refs.mins.textContent = `${minutes}`;
-//   refs.secs.textContent = `${seconds}`;
-// }
-
-// function addLeadingZero(value) {
-//   return String(value).padStart(2, '0');
-// }
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   const days = addLeadingZero(Math.floor(ms / day));
-//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-//   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-
-//   return { days, hours, minutes, seconds };
 
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,83 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+// Завдання 3 - генератор промісів
+// Виконуй це завдання у файлах 03 - promises.html і 03 - promises.js. 
+// Подивися демо - відео роботи генератора промісів.
+
+// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку 
+// в мілісекундах, крок збільшення затримки для кожного промісу після першого 
+// і кількість промісів, яку необхідно створити.
+
+
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) 
+// стільки разів, скільки ввели в поле amount.
+// Під час кожного виклику передай їй номер промісу(position), що створюється,
+//   і затримку, враховуючи першу затримку(delay), введену користувачем, і крок(step).
+
+
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс,
+//   який виконується або відхиляється через delay часу.Значенням промісу повинен бути об'єкт, 
+//   в якому будуть властивості position і delay зі значеннями однойменних параметрів.
+//   Використовуй початковий код функції для вибору того, що потрібно зробити з промісом -
+//   виконати або відхилити.
+
+
+
+// Бібліотека повідомлень
+// УВАГА
+// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+// Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
+
+
+const pageForm = document.querySelector('.form')
+
+
+const onBtnclick = (event) => {
+  event.preventDefault();
+  let delay = Number(event.currentTarget.delay.value);
+  const step = Number(event.currentTarget.step.value);
+  const amount = Number(event.currentTarget.amount.value);
+  let position = 0;
+  console.log(delay);
+  console.log(step);
+  console.log(amount);  
+
+  for (let i = 1; i <= amount; i += 1) {
+    position = i;       
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
   }
+  
+}  
+
+pageForm.addEventListener('submit', onBtnclick);
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    console.log(position);
+    console.log(delay);
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay }) // Fulfill
+      } else {
+        reject({ position, delay }) // Reject
+      }
+    }, delay)      
+  })
+  
+    
+
+
+
 }
+
+
